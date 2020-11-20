2a2fa7093769e7c9430c3694b7de5f4f3487fc9f
Jai Malani <71269258+jai38@users.noreply.github.com>
2020-11-21 02:37:44 +0530

    login part done

--- Login/JS/login.js
+++ Login/JS/login.js
@@ -1,7 +1,32 @@
 let flag = false;
+var i;
+var currentUser;
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('login'.addEventListener('click',checkUser()));
+    document.getElementById('login').addEventListener('click',checkUser);
 });
-const checkUser = () => {
-    
+const checkUser = (ev) => {
+    ev.preventDefault();
+    currentUser = {
+        Username: document.getElementById('username').value,
+        Password: document.getElementById('password').value
+    };
+    let allUserDetails =JSON.parse("["+localStorage.getItem('allUserDetails')+"]");
+    for(i = 1; i < allUserDetails.length; i++) {
+        console.log(allUserDetails[i]);
+        if(allUserDetails[i].Username == currentUser.Username && allUserDetails[i].Password == currentUser.Password) {
+            flag = true;
+            console.log(allUserDetails[i] + " " + i);
+            localStorage.setItem('loggedInUser',JSON.stringify(allUserDetails[i]));
+            location.href = "LoginPage.html";
+            break;
+        } else {
+            flag = false;
+        }
+    }
+    if(i==allUserDetails.length) {
+        alert('Incorrect Details');
+    }
+}
+const resetUser = () => {
+    localStorage.setItem('loggedInUser', " ");
 }
