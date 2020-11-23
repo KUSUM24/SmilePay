b90cf12e4b9b167dc4004c893e80a8364909e198
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2020-11-24 04:31:08 +0530

    half changes

--- Signup/JS/face.js
+++ Signup/JS/face.js
@@ -41,7 +41,7 @@ async function capture() {
     var data = canvas.toDataURL('image/jpg')
     i = parseInt(localStorage.getItem('imgNo'));
     localStorage.setItem(`img${i}`,data);
-    i--;
+    // i--;
     localStorage.setItem('imgNo',i.toString());
     console.log(i);
     if(i<=0){
@@ -64,10 +64,10 @@ async function capture() {
         }
         localStorage.setItem('allUserDetails',localStorage.getItem('allUserDetails')+","+JSON.stringify(currentUser));
         backupData();
-        window.location.href = 'finalSignup.html';
+        // window.location.href = 'finalSignup.html';
     }
     else{
-        window.alert('photos left:' + i.toString())
+        document.getElementById("box");
         location.reload();
         startVideo();
     }
